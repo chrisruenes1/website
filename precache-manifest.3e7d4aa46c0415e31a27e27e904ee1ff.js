61be5df7f552e94ba0496f63490a5980b3b8eb15
Chris Ruenes <ruenes.chris@gmail.com>
2021-12-14 01:57:34 -0500

    Updates

---
+++ precache-manifest.3e7d4aa46c0415e31a27e27e904ee1ff.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d88b0c02d2a009742f3",
+    "url": "/website/static/css/main.afe529cb.chunk.css"
+  },
+  {
+    "revision": "7d88b0c02d2a009742f3",
+    "url": "/website/static/js/main.7d88b0c0.chunk.js"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.d15da1c5.js"
+  },
+  {
+    "revision": "c56dd14c7a3e72e51228",
+    "url": "/website/static/js/2.c56dd14c.chunk.js"
+  },
+  {
+    "revision": "e8ed79a3e66cdf8989d5ec07b553aa55",
+    "url": "/website/static/media/chrome-error-screen-censored.e8ed79a3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e113a594d3a3b685641fe1fa2316532c",
+    "url": "/website/static/media/41-42.e113a594.mp3"
+  },
+  {
+    "revision": "840f48cbedbab4d974b2b4f194db826c",
+    "url": "/website/static/media/calamus-v.840f48cb.mp3"
+  },
+  {
+    "revision": "b13e28fe12f115b4746c94ea2e6023b7",
+    "url": "/website/static/media/car-scene.b13e28fe.mp3"
+  },
+  {
+    "revision": "e1fdcfd18f839498b9af8e344abe6870",
+    "url": "/website/static/media/car-scene.e1fdcfd1.wav"
+  },
+  {
+    "revision": "c4ec99fd13be37061c631393de2c2a16",
+    "url": "/website/static/media/kiss-scene.c4ec99fd.wav"
+  },
+  {
+    "revision": "d9d202700cafbba42616e398bb271931",
+    "url": "/website/static/media/kyrie.d9d20270.wav"
+  },
+  {
+    "revision": "96dd860ec888bc195e913028c3c3bc6c",
+    "url": "/website/static/media/melt.96dd860e.mp3"
+  },
+  {
+    "revision": "57a4a4afa17f5f14e8a42ffdbc502e6c",
+    "url": "/website/static/media/ruptures-1.57a4a4af.mp3"
+  },
+  {
+    "revision": "13de29a7f8e5c38fe55a2a4968c0e048",
+    "url": "/website/static/media/ruptures-2.13de29a7.mp3"
+  },
+  {
+    "revision": "d9adda55ae42fd30985294e457eab6a8",
+    "url": "/website/static/media/ruptures-3.d9adda55.mp3"
+  },
+  {
+    "revision": "e126bc9601f484961bc88f8a85336e9d",
+    "url": "/website/static/media/salt.e126bc96.mp3"
+  },
+  {
+    "revision": "b8349696b75df58171687f4d3f0839b2",
+    "url": "/website/static/media/tumbling-1.b8349696.mp3"
+  },
+  {
+    "revision": "893d370903a538d5bfc9f7d6d548832c",
+    "url": "/website/static/media/tumbling-2.893d3709.mp3"
+  },
+  {
+    "revision": "68b287d285a7fa6484602c33c10069c0",
+    "url": "/website/static/media/tumbling-3.68b287d2.mp3"
+  },
+  {
+    "revision": "70769886d91c0d44691ca83a17e3697c",
+    "url": "/website/static/media/tumbling-4.70769886.mp3"
+  },
+  {
+    "revision": "8515ecbab2f52d84797bdf2258cf6d93",
+    "url": "/website/index.html"
+  }
+];
